941360a090babb05be3ec88a6728db75b6499cd5
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-04-24 22:07:10 +0300

    tidy up

--- app/routers/user.py
+++ app/routers/user.py
@@ -8,10 +8,6 @@ router = APIRouter(
     tags=["users"]
 )
 
-#PostCreate is inbound request validation 
-#while UserOut is outbound response validation
-#db:Session ...is querying the database and give us final user
-#next to is where you add OAuth
 @router.post("/", status_code=status.HTTP_201_CREATED,response_model=schemas.UserOut)
 def create_user(user : schemas.UserCreate, db: Session = Depends(get_db)):
    #user is the validated data the schema returns
@@ -26,13 +22,9 @@ def create_user(user : schemas.UserCreate, db: Session = Depends(get_db)):
    return  new_user 
 
 
-#get user by id
-# 1st line is called a decorator
 @router.get('/{id}',response_model=schemas.UserOut)
 def get_user(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
    user = db.query(models.User).filter(models.User.id == id).first()
    if not user:
       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id{id} does not exits")
    return user
-
-
