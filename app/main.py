e6141001be93d134dc1f17d399c8a97fcf56e9c3
Eyuel-Nigussie <eyuthedev@gmail.com>
2023-05-01 19:20:41 +0300

    websocket added

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 #mealapp
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, Depends
 from . import  models
 from .database import engine 
 from .routers import user,auth,recipe,ingredient
 from fastapi.middleware.cors import CORSMiddleware
 
+from sqlalchemy.orm import Session
+from . import models, schemas, utils, oauth2
+from .database import engine , get_db
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -33,3 +36,27 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(recipe.router)
 app.include_router(ingredient.router)
+
+
+
+#websocket to get ingredients in user's shopping list
+@app.websocket('/ws/shopping')
+async def websocket_get_shopping_list(websocket: WebSocket, db: Session = Depends(get_db)):
+    await websocket.accept()
+
+    # Retrieve all Shopping objects for the current user
+    print('hello')
+    shopping_list = db.query(models.Shopping).filter_by(user_id=1).all()
+
+    # Create an empty list to hold the ingredients
+    ingredients = []
+
+    # Map the ingredient_id foreign key to the ingredients table
+    for item in shopping_list:
+        ingredient = db.query(models.Ingredient).get(item.ingredient_id)
+        if ingredient:
+            # Append the ingredient to the list
+            ingredients.append(ingredient)
+
+    # Send the list of ingredients in the shopping list over the websocket
+    await websocket.send_json({"ingredients": [schemas.IngredientCreate.from_orm(ing).dict() for ing in ingredients]})
